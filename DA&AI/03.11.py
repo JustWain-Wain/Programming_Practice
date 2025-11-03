ff9af272fcc2a1b55d71c2789df7cbd0688bbdac
JustWain <goshagoog@gmail.com>
2025-11-03 09:26:00 +0500

    Update 03.11.py

--- DA&AI/03.11.py
+++ DA&AI/03.11.py
@@ -1,15 +1,11 @@
 import torch
-import transformers
-
 from transformers import pipeline
+from typing import Literal
 
-classifier = pipeline("sentiment-analysis")
+classifier = pipeline(Literal["sentiment-analysis"])
 classifier(
     [
         "I've been ready for a HuggingFace course my whole life.",
         "I hate this so much!",
     ]
 )
-classifier = pipeline("text-classification",
-                     model="DeepPavlov/distilrubert-small-cased-conversational")
-results = classifier("Эта игра просто великолепна!")
